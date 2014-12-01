2cb50036db8ffb5c85605a520a2e2c9387451eb0
Jays85 <519850227@qq.com>
2014-12-01 20:00:07 +0800

    add

---
+++ WebApplication2/WebApplication2/base1.js
@@ -0,0 +1,324 @@
+﻿//前台用$()来实例化一个新对像
+var $ = function (args) {
+    return new Base(args);
+ }
+function Base(args) {
+    this.Ems = [];
+    debugger;
+     if (typeof args == 'string') {
+         if (args.indexOf != -1) {
+             var tempelements = args.split(' ');
+             var childnode = [];
+             var childelement = [];
+             for (var i = 0; i < tempelements.length; i++) {
+                 if (childnode.length == 0) {
+                     childnode.push(document);
+                 }
+                 switch (tempelements[i].charAt(0)) {
+                     case '#':
+                         childelement = [];
+                         childelement.push(this.getId(tempelements[i].substring(1)));
+                         childnode = childelement;
+                         break;
+                     case '.':
+                         childelement = [];
+                         for (var j = 0; j < childnode.length; j++) {
+                             var temps = this.getClass(tempelements[i].substring(1), childnode[j]);
+                             for (var k = 0; k < temps.length; k++) {
+                                 childelement.push(temps[k]);
+                             }
+                         }
+                         childnode = childelement;
+                         break;
+                     default:
+                         childelement = [];
+                         for (var j = 0; j < childnode.length; j++) {
+                             var temps = this.getTagName(tempelements[i], childnode[j]);
+                             for (var k = 0; k < temps.length; k++) {
+                                 childelement.push(temps[k]);
+                             }
+                         }
+                         childnode = childelement;
+                  }
+             }
+             this.Ems = childelement ;
+         }
+         else {
+             switch (args.charAt(0)) {
+                 case '#':
+                     this.Ems.push(this.getId(args.substring(1)));
+                     break;
+                 case '.':
+                     this.getClassName(args.substring(1));
+                     break;
+                 default:
+                     this.Ems = this.getTagName(args);
+             } 
+         }
+     }
+     else if (typeof args == 'object' && args != undefined) {
+         this.Ems[0] = args;
+     }
+     else if (typeof args == 'function') {
+		this.ready(args);
+	}
+
+}
+//第一个元素
+Base.prototype.first = function () {
+    return this.Ems[0];
+}
+//最后一个元素
+Base.prototype.last = function () {
+    return this.Ems[this.Ems.length - 1];
+}
+ //addDomLoaded
+ Base.prototype.ready = function (fn) {
+     addDomLoaded(fn);
+ };
+ Base.prototype.find = function (str) {
+     var childelement = [];
+     for (var i = 0; i < this.Ems.length; i++) {
+         switch (str.charAt(0)) {
+             case '#':
+                 childelement.push(this.getId(str.substring(1)));
+                 break;
+             case '.':
+                 var temps = this.getClass(str.substring(1), this.Ems[i]);
+                 for (var j = 0; j < temps.length; j++) {
+                     childelement.push(temps[j]);
+                 }
+                 break;
+             default:
+                 var temps = this.getTagName(str, this.Ems[i]);
+                 for (var k = 0; k < temps.length; k++) {
+                     childelement.push(temps[k]);
+                 }
+                 break;
+         }
+     }
+     this.Ems = childelement;
+     return this;
+ }
+ //跟据ID获取元素
+ Base.prototype.getId = function (id) {
+     return document.getElementById(id);
+ };
+ //跟据标签名获取元素
+ Base.prototype.getTagName = function (TagName, parentnode) {
+     var node;
+     var temps = [];
+     if (arguments.length == 1) {
+         TagNames = document.getElementsByTagName(TagName);
+     }
+     else {
+         if (parentnode != undefined && parentnode != 'undefined') {
+             node = parentnode;
+         }
+         else {
+             node = document;
+         }
+         var TagNames = parentnode.getElementsByTagName(TagName);
+     }
+
+     for (var i = 0; i < TagNames.length; i++) {
+         temps.push(TagNames[i]);
+     }
+     return temps;
+ }
+ //删除属性
+ Base.prototype.delClass = function (ClassName) {
+     for (var i = 0; i < this.Ems.length; i++) {
+         this.Ems[i].removeAttribute(ClassName);
+     }
+     return this;
+ }
+ //添加删除Class属性
+ Base.prototype.addClass = function (ClassName, ClassValue) {
+     for (var i = 0; i < this.Ems.length; i++) {
+         if (arguments.length == 1) {
+             this.Ems[i].removeAttribute(ClassName);
+         }
+         else {
+             this.Ems[i].setAttribute(ClassName, ClassValue);
+         }
+     }
+     return this;
+ }
+ //获取元素集合中指定元素
+ Base.prototype.getElement = function (num) {
+     if (arguments.length == 1) {
+         var element = this.Ems[num];
+         this.Ems = [];
+         this.Ems[0] = element;
+         return this;
+     }
+     else {
+         return this;
+     }
+ }
+ //跟据class类名获取元数
+ Base.prototype.getClassName = function (ClassName, id) {
+     var TagNames
+     if (arguments.length == 1) {//只有一个属性就所有ClassName元素
+         TagNames = document.getElementsByTagName('*');
+     }
+     else {//否则跟据ID获取ID区块下所有ClassName元素
+         TagNames = document.getElementById(Id).getElementsByTagName('*');
+     }
+     for (var i = 0; i < TagNames.length; i++) {
+         if (TagNames[i].className == ClassName) {
+             this.Ems.push(TagNames[i]);
+         }
+     }
+     return this;
+ }
+ //跟据class类名获取元数
+ Base.prototype.getClass = function (ClassName, parentnode) {
+     var TagNames;
+     var node;
+     var temps = [];
+     if (arguments.length == 1) {//只有一个属性就所有ClassName元素
+         TagNames = document.getElementsByTagName('*');
+     }
+     else {
+         if (parentnode != undefined) {
+             node = parentnode;
+         }
+         else {
+             node = document;
+         }
+         TagNames = parentnode.getElementsByTagName('*');
+     }
+     for (var i = 0; i < TagNames.length; i++) {
+         if (TagNames[i].className == ClassName) {
+             temps.push(TagNames[i]);
+         }
+     }
+     return temps;
+ }
+//鼠标移动事件
+Base.prototype.Hover = function (over, out) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        addEvent(this.Ems[i], 'mouseover', over);
+        addEvent(this.Ems[i], 'mouseout', out);
+    }
+    return this;
+}
+//显示指定区块
+Base.prototype.show = function () {
+    for (var i = 0; i < this.Ems.length; i++) {
+        this.Ems[i].style.display = 'block';
+    }
+    return this;
+}
+
+//设置隐藏区块
+Base.prototype.hide = function () {
+    for (var i = 0; i < this.Ems.length; i++) {
+        this.Ems[i].style.display = 'none';
+    }
+    return this;
+}
+
+//添加删除外联CSS规则
+Base.prototype.addRule = function (num, cssrule, csstext, position) {
+    var sheetstyle = document.styleSheets[num];
+    if (arguments.length == 2) {
+        tDeleteRule(sheetstyle, position);
+    }
+    else if (arguments.length == 4) {
+        tAddRule(sheetstyle,cssrule,csstext,position);
+    }
+    return this;
+}  //设置元素居中
+Base.prototype.setCenter = function (width, height) {
+    var top = (tGetInner().height - height) / 2;
+    var left = (tGetInner().width - width) / 2;
+        for (var i = 0; i < this.Ems.length; i++) {
+            this.Ems[i].style.top = top + 'px';
+            this.Ems[i].style.left = left + 'px';
+    }
+    return this;
+}
+//窗体改变事件
+Base.prototype.reSize = function (fn) {
+//    window.onresize = fn;
+    addEvent(window,'resize',fn);
+    return this;
+}
+//设置行内CSS样式
+Base.prototype.Css = function (attr, value) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        if (arguments.length == 1) {//判断只有一个属性那么就取读CSS属性值
+            return tCss(this.Ems[i], attr);
+        }
+        else {
+            this.Ems[i].style[attr] = value;
+        }
+    }
+    return this;
+}
+//添加单击方法
+Base.prototype.Click = function (fn) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        addEvent(this.Ems[i], 'click', fn);
+    }
+    return this;
+}
+//设置元素文本内容
+Base.prototype.Html = function (str) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        if (arguments.length == 0) {
+            return this.Ems[i].innerHTML;
+        }
+        this.Ems[i].innerHTML = str;
+    }
+    return this;
+}
+//设置锁屏
+Base.prototype.operationLock = function (status) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        if (status == 'lock') {
+            this.Ems[i].style.width = tGetInner().width + 'px';
+            this.Ems[i].style.height = tGetInner().height + 'px';
+            this.Ems[i].style.display = 'block';
+            document.documentElement.style.overflow = 'hidden';
+        }
+        else if (status == 'unlock') {
+            this.Ems[i].style.display = 'none';
+            document.documentElement.style.overflow = 'auto';
+        }
+    }
+    return this;
+}
+//移动动画
+Base.prototype.animate = function (obj) {
+    for (var i = 0; i < this.Ems.length; i++) {
+        var element = this.Ems[i];
+        var attr = obj['attr'] != undefined ? obj['attr'] : 'left';
+        var step = obj['step'] != undefined ? obj['step'] : 10;
+        var t = 50;
+        var start = obj['start'] != undefined ? obj['start'] : tCss(element, attr);
+        var target = obj['alter'] + start;
+        if (start > target) setp = -setp;
+        element.style[attr] = start + 'px';
+        clearInterval(window.timer);
+        timer = setInterval(function () {
+            element.style[attr] = tCss(element, attr) + step + 'px';
+            if (step > 0 && tCss(element, attr) >= target) {
+                element.style[attr] = target + 'px';
+                clearInterval(timer);
+            } else if (step < 0 && tCss(element, attr) <= target) {
+                element.style[attr] = target + 'px';
+                clearInterval(timer);
+            }
+        }, t);
+    }
+    return this;
+}
+//调用插件
+Base.prototype.Extend = function (name, fn) {
+    Base.prototype[name] = fn;
+}
+
---
+++ WebApplication2/WebApplication2/tools.js
@@ -0,0 +1,187 @@
+﻿
+//浏览器检测
+(function () {
+    window.sys = {};
+    var ua = navigator.userAgent.toLowerCase();
+    var s;
+    (s = ua.match(/msie ([\d.]+)/)) ? sys.ie = s[1] :
+	(s = ua.match(/firefox\/([\d.]+)/)) ? sys.firefox = s[1] :
+	(s = ua.match(/chrome\/([\d.]+)/)) ? sys.chrome = s[1] :
+	(s = ua.match(/opera\/.*version\/([\d.]+)/)) ? sys.opera = s[1] :
+	(s = ua.match(/version\/([\d.]+).*safari/)) ? sys.safari = s[1] : 0;
+})();
+
+//DOM加载
+function addDomLoaded(fn) {
+    var isReady = false;
+    var timer = null;
+    function doReady() {
+        if (timer) clearInterval(timer);
+        if (isReady) return;
+        isReady = true;
+        fn();
+    }
+
+    if ((sys.opera && sys.opera < 9) || (sys.firefox && sys.firefox < 3) || (sys.webkit && sys.webkit < 525)) {
+        //无论采用哪种，基本上用不着了
+        /*timer = setInterval(function () {
+        if (/loaded|complete/.test(document.readyState)) { 	//loaded是部分加载，有可能只是DOM加载完毕，complete是完全加载，类似于onload
+        doReady();
+        }
+        }, 1);*/
+
+        timer = setInterval(function () {
+            if (document && document.getElementById && document.getElementsByTagName && document.body) {
+                doReady();
+            }
+        }, 1);
+    } else if (document.addEventListener) {//W3C
+        addEvent(document, 'DOMContentLoaded', function () {
+            fn();
+            removeEvent(document, 'DOMContentLoaded', arguments.callee);
+        });
+    } else if (sys.ie && sys.ie < 9) {
+        var timer = null;
+        timer = setInterval(function () {
+            try {
+                document.documentElement.doScroll('left');
+                doReady();
+            } catch (e) { };
+        }, 1);
+    }
+}
+
+//兼容IE和非IE删除外联CSS文件中的样式
+function tDeleteRule(sheetstyle,position) {
+    if (typeof sheetstyle.deleteRule != 'undefined') {
+        sheetstyle.deleteRule(position);
+    }
+    else {
+        sheetstyle.removeRule(position);
+    }
+}
+//兼容IE和非IE添加外联CSS文件中的样式
+function tAddRule(sheetstyle,cssrule,csstext,position) {
+    if (typeof sheetstyle.insertRule != 'undefined') {
+        var cssstr = cssrule + '{' + csstext + '}';
+        sheetstyle.insertRule(cssstr, position);
+    }
+    else {
+        sheetstyle.addRule(cssrule, csstext, position);
+    }
+}
+//兼容IE非IE获取行内，外连CSS属性值
+function tCss(element, attr) {
+    var value;
+    if (typeof window.getComputedStyle != 'undefined') {//判断非IE方法是否合法，如果合法就调用
+        value=parseInt(window.getComputedStyle(element, null)[attr]);
+    }
+    else if (typeof element.currentStyle != 'undefined') {//判断IE方法是否合法，如果合法调用
+        value=parseInt(element.currentStyle[attr]);
+    }
+    else {
+        value=parseInt(element.style[attr]); //读取行内style
+    }
+    return value;
+}
+//兼容IE非IE获取视窗大小
+function tGetInner() {
+    if (typeof window.innerWidth != 'undefined') {
+        return {
+            width: window.innerWidth, 
+            height: window.innerHeight
+        }
+    }
+    else{
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }
+    }
+}
+//兼容IE非IE获取事件
+function tGetEvent(event) {
+    return event || window.event;
+}
+//跨浏览器添加事件绑定
+function addEvent(obj, type, fn) {
+    if (typeof obj.addEventListener != 'undefined') {
+        obj.addEventListener(type, fn, false);
+    } else {
+        //创建一个存放事件的哈希表(散列表)
+        if (!obj.events) obj.events = {};
+        //第一次执行时执行
+        if (!obj.events[type]) {
+            //创建一个存放事件处理函数的数组
+            obj.events[type] = [];
+            //把第一次的事件处理函数先储存到第一个位置上
+            if (obj['on' + type]) obj.events[type][0] = fn;
+        }
+        else {
+            if (addEvent.equal(obj.events[type], fn)) {
+                return false;
+             }
+        }
+        //从第二次开始我们用事件计数器来存储
+        obj.events[type][addEvent.ID++] = fn;
+        //执行事件处理函数
+        obj['on' + type] = addEvent.exec;
+    }
+}
+//匹配默认属性和方法
+addEvent.fixEvent = function (event) {
+    event.preventDefault = addEvent.fixEvent.preventDefault;
+    event.stopPropagation = addEvent.fixEvent.stopPropagation;
+    event.target = event.srcElement;
+    return event;
+}
+ 
+addEvent.fixEvent.preventDefault = function () {
+    this.returnValue = false;
+}
+addEvent.fixEvent.stopPropagation = function () {
+    this.cancelBubble = true;
+}
+
+//为每个事件分配一个计数器
+addEvent.ID = 1;
+//执行所有事件
+addEvent.exec = function (evt) {
+    var e = evt || addEvent.fixEvent(window.event);
+    var es = this.events[e.type];
+    for (var i in es) {
+        es[i].call(this, e);
+    }
+}
+//判断事件是否已存在
+addEvent.equal = function (es, fn) {
+    for (var i in es) {
+        if (es[i] == fn) {
+            return true;
+        }
+    }
+    return false;
+}
+//跨浏览器删除事件
+function removeEvent(obj, type, fn) {
+    if (typeof obj.removeEventListener != 'undefined') {
+        obj.removeEventListener(type, fn, false);
+    } else {
+        for (var i in obj.events[type]) {
+            if (obj.events[type][i] == fn) {
+                delete obj.events[type][i];
+             }
+           }
+    }
+}
+//获取事件元素
+function getTarget(evt) {
+
+    var e = event || window.event;
+    if (e.target) {
+        return e.target;
+    }
+    else if (e.srcElement) {
+        return e.srcElement;
+    }
+}
